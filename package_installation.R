3c7310794a6fc82dfb2be2cb82e0f6914a850c2d
Tim Thurman <timothy.j.thurman@gmail.com>
2017-09-19 17:13:58 -0400

    new package installation script, typo correct

---
+++ package_installation.R
@@ -0,0 +1,9 @@
+## R script for installing a bunch of packages on Cedar
+## could possibly run this using the Rscript command,
+## but probably easier to just do it interactively.
+
+install.packages("tidyverse")
+install.packages("broom")
+install.packages("car")
+install.packages("foreach")
+install.packages("doParallel")
